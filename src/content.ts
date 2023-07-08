bb2395bd7988c757c2e531d6513b93528fc372c4
Deka <dekachan16@gmail.com>
2023-07-08 14:01:52 +0900

    fix: Fix css class name

--- src/content.ts
+++ src/content.ts
@@ -1,6 +1,6 @@
 import { debounce } from "ts-debounce";
 import browser from "webextension-polyfill";
-import { notNull, retry } from "./utils";
+import { retry } from "./utils";
 import "./style.css";
 
 const RETRY_COUNT = 3;
@@ -54,78 +54,55 @@ const startTimer = async (description: string, habit: string) => {
 };
 
 const getRoot = async () => {
-  return retry(() => document.querySelector<HTMLElement>("#root > div"));
+  return retry(() => document.querySelector<HTMLElement>("#root"));
 };
 
-const getHabitLists = async () => {
-  const todo = await retry(() =>
-    document.querySelector<HTMLElement>(
-      "#root > div > div.main-journal > div.css-wdd64 > div.css-11btfhh > div.css-0"
-    )
+const getHabitElements = async () => {
+  return await retry(() =>
+    document.querySelectorAll<HTMLElement>(".item-habit-info")
   );
-  const done = await retry(() =>
-    document.querySelector<HTMLElement>(
-      "#root > div > div.main-journal > div.css-wdd64 > div.css-11btfhh > div:nth-child(2) > div.css-0 > div.chakra-collapse > div.css-0"
-    )
-  );
-  const skip = await retry(() =>
-    document.querySelector<HTMLElement>(
-      "#root > div > div.main-journal > div.css-wdd64 > div.css-11btfhh > div:nth-child(3) > div.css-0 > div.chakra-collapse > div.css-0"
-    )
-  );
-  return [todo, done, skip].filter(notNull);
 };
 
 const createTogglButton = (onclick: (e: MouseEvent) => void) => {
-  const button = document.createElement("div");
+  const button = document.createElement("button");
+  button.type = "button";
   button.className = "toggl-button";
   button.onclick = onclick;
   return button;
 };
 
-const appendTogglButton = ($item: HTMLElement) => {
-  if ($item.querySelector(".toggl-button")) return;
-  const $info = $item.querySelector(".item-habit-info");
-  $info?.insertBefore(
+const appendTogglButton = (item: HTMLElement) => {
+  if (item.querySelector(".toggl-button")) return;
+  item.insertBefore(
     createTogglButton((e) => {
       e.preventDefault();
       e.stopPropagation();
-      const habit = $item.querySelector(":scope > div")?.id;
-      const description = $item.querySelector(".habit-name")?.textContent;
+      const habit = item.parentElement?.id;
+      const description = item.querySelector(".habit-name")?.textContent;
       if (!habit || !description) {
         console.log("Failed to get habit or description: ", habit, description);
         return;
       }
       startTimer(description, habit);
     }),
-    $item.querySelector(".css-515xy0")
+    item.querySelector(".css-0")?.nextSibling ?? null
   );
 };
 
-const appendTogglButtons = debounce(async ($lists: HTMLElement[]) => {
-  for (const $list of $lists) {
-    const $items = $list.querySelectorAll<HTMLElement>(":scope > div");
-    for (const $item of $items) {
-      appendTogglButton($item);
-    }
-  }
-});
-
-const setupHabitListObserver = debounce(async () => {
-  const $lists = await getHabitLists();
-  appendTogglButtons($lists);
-  const observer = new MutationObserver(() => appendTogglButtons($lists));
-  for (const $list of $lists) {
-    observer.observe($list, { childList: true });
+const setupTogglButtons = debounce(async () => {
+  const habits = await getHabitElements();
+  if (!habits) return;
+  for (const habit of habits) {
+    appendTogglButton(habit);
   }
 });
 
 const setupRootObserver = debounce(async () => {
-  setupHabitListObserver();
-  const $root = await getRoot();
-  if (!$root) return;
-  const observer = new MutationObserver(() => setupHabitListObserver());
-  observer.observe($root, { childList: true });
+  const root = await getRoot();
+  if (!root) return;
+  setupTogglButtons();
+  const observer = new MutationObserver(() => setupTogglButtons());
+  observer.observe(root, { childList: true, subtree: true });
 });
 
 const initialize = async () => {
